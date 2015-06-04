163459b8d98a243de41f91b8c74e8be7bae5e8f3
Dillon Hafer <dh@dillonhafer.com>
2015-06-04 11:51:26 -0400

    Add mkdir command to imdb

--- Formula/imdb-tags.rb
+++ Formula/imdb-tags.rb
@@ -9,6 +9,7 @@ class ImdbTags < Formula
 
   def install
     ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace"
+    system("mkdir -p #{buildpath}/Godeps/_workspace/src/github.com/dillonhafer/imdb-tags")
     system("go build -o imdb-tags")
     bin.install("imdb-tags")
   end
