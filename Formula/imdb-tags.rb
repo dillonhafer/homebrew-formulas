4c160c197c5f1e2c6f7e08d7f9328cb385afcb19
Dillon Hafer <dh@dillonhafer.com>
2015-06-04 11:46:47 -0400

    Add sha

--- Formula/imdb-tags.rb
+++ Formula/imdb-tags.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ImdbTags < Formula
   homepage "http://imdb-tags.dillonhafer.com"
   url "https://github.com/dillonhafer/imdb-tags/archive/v0.4.0.tar.gz"
-  sha1 ""
+  sha1 "2c99625595f523325734c59686ca7ee82daad94d2b8dc59bd2df4523b28544ef"
 
   depends_on 'go' => :build
 
