2b1f98f1943042b1e3fb27d390fbad9e60cf8572
Dillon Hafer <dh@dillonhafer.com>
2015-06-04 12:11:43 -0400

    Fix

--- Formula/imdb-tags.rb
+++ Formula/imdb-tags.rb
@@ -8,7 +8,7 @@ class ImdbTags < Formula
   depends_on 'go' => :build
 
   def install
-    ENV["GOPATH"] = buildpath
+    #ENV["GOPATH"] = buildpath
     system("go build -o imdb-tags")
     bin.install("imdb-tags")
   end
