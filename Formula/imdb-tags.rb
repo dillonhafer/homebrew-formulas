d0a7c75e4cebea19a1cce1636742244fccdb05e7
Dillon Hafer <dh@dillonhafer.com>
2015-06-04 12:00:09 -0400

    Fix install method

--- Formula/imdb-tags.rb
+++ Formula/imdb-tags.rb
@@ -9,8 +9,6 @@ class ImdbTags < Formula
 
   def install
     ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace"
-    system("mkdir -p #{buildpath}/Godeps/_workspace/src/github.com/dillonhafer/imdb-tags")
-    system("mv #{buildpath} #{buildpath}/Godeps/_workspace/src/github.com/dillonhafer/imdb-tags")
     system("go build -o imdb-tags")
     bin.install("imdb-tags")
   end
